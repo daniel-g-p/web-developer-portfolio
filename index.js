7a4ac13b5da07833988c3b4bc0d99bd736f76e4b
Daniel <danielgiustiniperez@gmail.com>
2021-04-30 11:47:37 +0200

    Uploaded all server-side files

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+require("dotenv").config();
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/", async(req, res) => {
+    const data = req.body;
+    const mailOptions = {
+        from: data.email,
+        to: "danielgiustiniperez@gmail.com",
+        subject: `Message from ${data.name}: ${data.subject}`,
+        text: data.message + `Email Address: ${data.email}`
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.json({ status: "error" });
+        } else {
+            res.json({ status: "success" });
+        };
+    });
+});
+
+app.use((req, res) => {
+    res.redirect("/");
+});
+
+app.listen(8080, () => {
+    console.log("Port 8080: Open");
+});
